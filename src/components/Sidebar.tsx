1c70ee005debda22c5fd96365474ee18399d69e6
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-04-08 01:27:39 +0530

    ui updated #5

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,8 +3,10 @@ import React from 'react'
 function Sidebar() {
   return (
     <section className='stcky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
-        <div className='flex flex-1 flex-col gap-6'>
-
+        <div className='flex flex- flex-col gap-6'>
+            {sidebarLinks.map((link) => (
+                <div></div>
+            ))}
         </div>
     </section>
   )
