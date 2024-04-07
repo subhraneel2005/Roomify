97497ebf3f42e289e72a965ca36bdd86a76ca1eb
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-04-08 01:06:37 +0530

    ui sidebar #4

--- src/app/(root)/(home)/layout.tsx
+++ src/app/(root)/(home)/layout.tsx
@@ -1,11 +1,13 @@
+import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar';
 import React, { ReactNode } from 'react'
 
 function HomeLayout({children} : {children : ReactNode}) {
   return (
     <main className='relative'>
-      Navbar
+      <Navbar/>
       <div className='flex'>
-        Sidebar
+        <Sidebar/>
       </div>
 
       <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <section className='stcky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
+        <div className='flex flex-1 flex-col gap-6'>
+
+        </div>
+    </section>
   )
 }
 
