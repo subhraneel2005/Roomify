c078f5c288c7d89d4e3b3b33dd16d521c4d6cf99
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-04-08 06:32:09 +0530

    Navbar build started

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+      <Link href={"/"} className='flex items-center gap-1'>
+        <Image
+          src={"/icons/logo.svg"}
+          width={32}
+          height={32}
+          className='max-sm:size-10'
+          alt='Roomify logo'/>
+        <p className='text-[27px] font-extrabold text-white max-sm:hidden'>Roomify</p>
+      </Link>
+
+      <div className='flex-between gap-5'>
+
+      </div>
+    </nav>
   )
 }
 
