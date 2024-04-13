97aba4cdbfc0c4e519043e533dc8e56524553f08
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-04-14 00:15:25 +0530

    bug fix #2

---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignUpButton, SignedIn } from '@clerk/nextjs'
+import React from 'react'
+
+function SignUp() {
+  return (
+    <div><SignUpButton/></div>
+  )
+}
+
+export default SignUp
